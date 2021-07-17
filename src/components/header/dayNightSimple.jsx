1579d51420cb0ecb2792f79cf7a60f5f8e8c43a0
dowon938 <dowon938@naver.com>
2021-07-17 22:39:46 +0900

    DayNightSimple Icon Fin

--- src/components/header/dayNightSimple.jsx
+++ src/components/header/dayNightSimple.jsx
@@ -4,106 +4,24 @@ import styles from './dayNightSimple.module.css';
 const DayNightSimple = memo(({ darkTheme, themeToggle }) => {
   return (
     <div className={styles.container} onClick={themeToggle}>
-      <div
-        className={styles.sunContainer}
-        style={{
-          opacity: darkTheme ? 0 : 1,
-        }}
-      >
-        <div className={styles.sun}></div>
-        <div
-          className={`${styles.ray} ${styles.long} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(0deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.short} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(30deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.long} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(60deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.short} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(90deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.long} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(120deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.short} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(150deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.long} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(180deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.short} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(210deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.long} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(240deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.short} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(270deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.long} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(300deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
-        <div
-          className={`${styles.ray} ${styles.short} `}
-          style={{
-            transform: darkTheme ? '' : 'rotate(330deg)',
-            opacity: darkTheme ? 0 : 1,
-          }}
-        ></div>
+      <div className={`${styles.sun} ${darkTheme ? styles.up : styles.down}`}></div>
+      {/* rays */}
+      <div className={`${styles.rays} ${darkTheme ? styles.down : styles.up}`}>
+        <div className={`${styles.ray} ${styles.deg0} ${styles.long} `}></div>
+        <div className={`${styles.ray} ${styles.deg36} ${styles.short} `}></div>
+        <div className={`${styles.ray} ${styles.deg72} ${styles.long} `}></div>
+        <div className={`${styles.ray} ${styles.deg108} ${styles.short} `}></div>
+        <div className={`${styles.ray} ${styles.deg144} ${styles.long} `}></div>
+        <div className={`${styles.ray} ${styles.deg180} ${styles.short} `}></div>
+        <div className={`${styles.ray} ${styles.deg216} ${styles.long} `}></div>
+        <div className={`${styles.ray} ${styles.deg252} ${styles.short} `}></div>
+        <div className={`${styles.ray} ${styles.deg288} ${styles.long} `}></div>
+        <div className={`${styles.ray} ${styles.deg324} ${styles.short} `}></div>
       </div>
-      <div
-        className={styles.moonContainer}
-        style={{
-          opacity: darkTheme ? 1 : 0,
-        }}
-      >
-        <div className={styles.moon}></div>
-        <div className={styles.cover}></div>
+      {/* moon */}
+      <div className={`${styles.cover} ${darkTheme ? styles.up : styles.down}`}></div>
+      <div className={`${styles.moonIcon} ${darkTheme ? styles.up : styles.down}`}>
+        <i className="fas fa-moon"></i>
       </div>
     </div>
   );
